14083cdae9a6ee01280f54fa0627c551e1d750f1
Shossain97 <shafeen.hossain97@gmail.com>
2017-11-16 20:55:19 -0600

    Basic wikipedia functionality no images

--- Wikipedia/Wiki.py
+++ Wikipedia/Wiki.py
@@ -1,6 +1,12 @@
 import discord
 from discord.ext import commands
 
+try:
+    import wikipedia
+    exists=True
+except:
+    exists=False
+
 
 
 class Wiki:
@@ -12,8 +18,15 @@ class Wiki:
     @commands.command()
     async def wiki(self, *, request : str):
         """Search Wikipedia's massive database"""
-
+        try:
+            answer=wikipedia.summary(request)
+            await self.bot.say(answer)
+        except:
+            await self.bot.say("no results")
         #insert wikipedia connection code
 
-def setup(bot)
-    bot.add_cog(Wiki(bot))
+def setup(bot):
+    if exists:
+        bot.add_cog(Wiki(bot))
+    else:
+        raise RuntimeError("You need to run `pip3 install wikipedia`")
