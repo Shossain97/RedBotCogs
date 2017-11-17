9119eb79272d683b90bbf1fa1dc849997aea905c
Shossain97 <shafeen.hossain97@gmail.com>
2017-11-16 18:56:34 -0600

    Initial wikipedia bot skeleton code

--- Wikipedia/Wiki.py
+++ Wikipedia/Wiki.py
@@ -1,2 +1,19 @@
 import discord
 from discord.ext import commands
+
+
+
+class Wiki:
+    """search wikipedia"""
+
+
+    def __init__(self, bot):
+        self.bot = bot
+    @commands.command()
+    async def wiki(self, *, request : str):
+        """Search Wikipedia's massive database"""
+
+        #insert wikipedia connection code
+
+def setup(bot)
+    bot.add_cog(Wiki(bot))
